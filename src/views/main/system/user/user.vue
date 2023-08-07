2df3b63c7cb789060dc918b4171a5851c4f8062c
smlz009 <1259826159@qq.com>
2023-08-07 11:31:49 +0800

    ✨ feat(user): 新增页面

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="user"></div>
+  <div class="user">
+    <div class="search-warp">
+      <user-search />
+    </div>
+    <div></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserSearch from './c-cpns/user-search.vue'
+</script>
 
 <style lang="less" scoped>
 .user {
